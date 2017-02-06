1c3f35590692d82c3667a97a75c156168e371647
xamfoo <xamfoo@users.noreply.github.com>
2017-02-06 10:38:40 +0800

    WIP

--- chapter-2/ex-2.20.cpp
+++ chapter-2/ex-2.20.cpp
@@ -1,5 +1,18 @@
 // Exercise 2.20: What does the following program do?
 
-// int i = 42;
-// int *p1 = &i;
-// *p1 = *p1 * *p1;
+// Answer: Creates a pointer to i = 42 and assigns the square of it via the
+// pointer.
+
+#include <iostream>
+
+int main ()
+{
+  int i = 42;
+  int *p1 = &i;
+  *p1 = *p1 * *p1;
+
+  std::cout << *p1 << '\n'
+            << i << std::endl;
+
+  return 0;
+}
---
+++ chapter-2/ex-2.21.md
@@ -0,0 +1,15 @@
+Exercise 2.21: Explain each of the following definitions. Indicate whether any
+are illegal and, if so, why.
+
+int i = 0;
+
+```
+(a) double* dp = &i;
+Illegal due to assigning a different pointer type.
+
+(b) int *ip = i;
+Legal because of initializing pointer as 0.
+
+(c) int *p = &i;
+Legal.
+```
